dc400afd76190b4d3480164eaf7d1a89ea3fe08a
tienshaoku <tienshaoku@gmail.com>
2024-11-17 15:25:16 +0700

    feat: uni swap_router

--- src/constant.rs
+++ src/constant.rs
@@ -1,2 +1,3 @@
 pub const GPv2SETTLEMENT: &str = "0x9008D19f58AAbD9eD0D60971565AA8510560ab41";
 pub const WETH: &str = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+pub const UNISWAP_V3_ROUTER: &str = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
